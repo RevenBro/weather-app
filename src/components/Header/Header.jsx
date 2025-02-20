2f65679727468e754a5b8d000c64e3a3ee742d6e
RevenBro <saidakbarov077@gmail.com>
2025-02-20 21:14:35 +0500

    Header section completed and responsived

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import Input from '../Input/Input'
 
 const Header = () => {
   return (
-    <div className='bg-[#252527] flex items-center justify-between p-5 h-[10%] gap-5 container'>
+    <div className='bg-[#252527] flex items-center justify-between p-2 md:p-5 h-[10%] sm:gap-2 gap-1 container mx-auto'>
       <div className='flex items-center justify-center gap-1'>
         <Map size={20} color='lightgreen'/>
-        <h2 className='text-[#feeeee] font-light '>
+        <h2 className='text-[#feeeee] font-light text-[7px] sm:text-[20px]'>
           <span className='font-medium'>London</span>, United Kingdom
         </h2>
       </div>
@@ -16,9 +16,8 @@ const Header = () => {
         <Search className='absolute top-[25%] left-[6px]' size={15} color='white'/>
         <Input/>  
       </div>
-      <div className='flex items-center gap-4'>
-        <Sun size={20} color='white' className='cursor-pointer'/>
-        <Settings size={20} color='white' className='cursor-pointer'/>
+      <div className='flex items-center sm:gap-3'>
+        <Sun color='white' className='cursor-pointer w-10 sm:w-20'/>
       </div>
     </div>
   )
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Input = () => {
   return (
-    <div className="relative w-44 my-3 mx-1 before:content-[''] before:absolute before:left-0 before:right-0 before:bottom-[-1px] before:border-b before:border-gray-600 before:transition-all before:duration-200 before:ease-in-out focus-within:before:border-green-400 focus-within:before:scale-x-100 after:content-[''] after:absolute after:left-0 after:right-0 after:bottom-[-1px] after:border-b-2 after:border-green-400 after:scale-x-0 after:transition-transform after:duration-250 focus-within:after:scale-x-100">
+    <div className="relative w-36 sm:w-44 my-2 sm:my-3 mx-1 before:content-[''] before:absolute before:left-0 before:right-0 before:bottom-[-1px] before:border-b before:border-gray-600 before:transition-all before:duration-200 before:ease-in-out focus-within:before:border-green-400 focus-within:before:scale-x-100 after:content-[''] after:absolute after:left-0 after:right-0 after:bottom-[-1px] after:border-b-2 after:border-green-400 after:scale-x-0 after:transition-transform after:duration-250 focus-within:after:scale-x-100">
       <input
         type="text"
         placeholder="Search"
-        className="w-full max-h-9 px-3 py-2 text-sm font-medium text-gray-200 bg-[#252527] shadow-md rounded-t-md border-none outline-none focus:bg-gray-700 placeholder:text-gray-400 transition-all duration-200"
+        className="w-full max-h-8 sm:max-h-9 px-3 py-1.5 sm:py-2 text-xs sm:text-sm font-medium text-gray-200 bg-[#252527] shadow-md rounded-t-md border-none outline-none focus:bg-gray-700 placeholder:text-gray-400 transition-all duration-200"
       />
     </div>
   );
