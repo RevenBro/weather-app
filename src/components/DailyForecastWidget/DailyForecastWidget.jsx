2e5ed1915cb59e67634b9a5354b2af21f0c3020c
RevenBro <saidakbarov077@gmail.com>
2025-02-24 18:12:12 +0500

    Daily weather info section completed

--- src/components/DailyForecastWidget/DailyForecastWidget.jsx
+++ src/components/DailyForecastWidget/DailyForecastWidget.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import WeatherIcon from '../WeatherIcon/WeatherIcon';
 
-const DailyForecastWidget = () => {
+const DailyForecastWidget = ({data}) => {
+  const {
+    day, icon, summary, temperature_max, temperature_min, precipitation
+  } = data;  
+
+  const now_date = {
+    day: new Intl.DateTimeFormat(navigator.language, {
+      weekday: "short",
+      day: "2-digit",
+      month: "2-digit"
+    }).format(new Date())
+  }
+
+  const weather_date = {
+    day: new Intl.DateTimeFormat(navigator.language, {
+      weekday: "short",
+      day: "2-digit",
+      month: "2-digit"
+    }).format(new Date(day))
+  }
+
+  weather_date.day = now_date.day === weather_date.day ? "Today" : weather_date.day
+  
   return (
-    <div>DailyForecastWidget</div>
+    <div className='my-10'>
+      <div className='flex flex-col gap-3 text-center'>
+        <p>{weather_date.day}</p>
+        <div className='rounded-3xl p-4 sm:p-7 border-green-400 border-[1.5px] flex flex-col items-center '>
+          <p className='text-[#F0EFEF] opacity-80 text-[14px]'>{}</p>
+          <WeatherIcon iconNumber={icon} summary={summary}/>
+          <p className='text-[#F0EFEF] opacity-80'>{Math.round(temperature_max)} °C</p>
+          <p className='text-[#F0EFEF] opacity-80'>{Math.round(temperature_min)} °C</p>
+          <div className='flex items-center justify-center mt-2'>
+            {/* <Moon width={15} height={15}/> */}
+            <p className='text-[#F0EFEF] opacity-80 text-[14px]'>{Math.round(precipitation.total)} mm/h</p>
+          </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
